b29143c120f3744d43090549eb04b1b3c337fa63
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2025-04-11 22:10:42 -0300

    Aula 05 - Testes Unitários

---
+++ semaphore/src/test/java/semaphore/light/e27/E27LightBulbTest.java
@@ -0,0 +1,30 @@
+package semaphore.light.e27;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import semaphore.light.Light;
+
+class E27LightBulbTest {
+
+	@Test
+	void shouldTurnOn() {
+		Light light = new E27LightBulb();
+
+		light.turnOff();
+		light.turnOn();
+
+		assertTrue(light.isOn());
+	}
+
+	@Test
+	void shouldTurnOff() {
+		Light light = new E27LightBulb();
+		
+		light.turnOn();
+		light.turnOff();
+
+		assertTrue(light.isOff());
+	}
+}
